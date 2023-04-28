950b2f2d3062d7a71194d0232b4b66cc800cc634
Claude216 <llxclaude126@gmail.com>
2023-04-28 11:24:20 -0700

    modified the base template and implement log in&out functions

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
 from .forms import PostForm
+from .models import Post
+
 
 # Create your views here.
+
+################ -------- POST -------- ################
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -39,5 +43,22 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+################ -------- LOG IN -------- ################
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'blog/login.html', {'error': 'Invalid credentials'})
+    else:
+        return render(request, 'blog/login.html')
 
+################ -------- LOG OUT -------- ################
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
